3130e91db9caf5e253c356fb63fb3f9123d69fff
nikhitashanker <rocketns@gmail.com>
2020-07-24 22:22:24 +0000

    Style fixes.

--- project/src/test/java/com/google/sps/VolunteerOpportunityTest.java
+++ project/src/test/java/com/google/sps/VolunteerOpportunityTest.java
@@ -2,8 +2,8 @@ package com.google.sps;
 
 import com.google.common.collect.ImmutableSet;
 import com.google.sps.data.VolunteeringOpportunity;
-import java.util.Set;
 import java.util.HashSet;
+import java.util.Set;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,13 +17,15 @@ public final class VolunteerOpportunityTest {
   private static final int NUMBER_OF_SPOTS = 240;
   private static final Set<String> SKILLS = ImmutableSet.of("Playing an instrument", "Performing");
   public static final Set<String> NEW_SKILLS = ImmutableSet.of("Singing", "Performing");
-  public static final Set<String> SKILLS_WITH_DANCING = ImmutableSet.of("Playing an instrument", "Performing", "Dancing");
+  public static final Set<String> SKILLS_WITH_DANCING =
+      ImmutableSet.of("Playing an instrument", "Performing", "Dancing");
   public static final String DANCING = "Dancing";
 
   @Test
   public void getAttributesAfterBuild() {
-    VolunteeringOpportunity opportunity =
-        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS).setRequiredSkills(SKILLS).build();
+    VolunteeringOpportunity opportunity = new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS)
+                                              .setRequiredSkills(SKILLS)
+                                              .build();
 
     String actualName = opportunity.getName();
     String expectedName = NAME;
@@ -39,10 +41,11 @@ public final class VolunteerOpportunityTest {
 
   @Test
   public void getAttributesWithToBuild() {
-    VolunteeringOpportunity opportunity =
-        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS).setRequiredSkills(SKILLS).build();
+    VolunteeringOpportunity opportunity = new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS)
+                                              .setRequiredSkills(SKILLS)
+                                              .build();
     VolunteeringOpportunity copyOfOpportunity = opportunity.toBuilder().build();
-    
+
     String actualName = copyOfOpportunity.getName();
     String expectedName = NAME;
     int actualNumSpotsLeft = copyOfOpportunity.getNumSpotsLeft();
@@ -52,7 +55,7 @@ public final class VolunteerOpportunityTest {
 
     Assert.assertEquals(actualName, expectedName);
     Assert.assertEquals(actualNumSpotsLeft, expectedNumSpotsLeft);
-    Assert.assertEquals(actualSkills,expectedSkills);
+    Assert.assertEquals(actualSkills, expectedSkills);
   }
 
   @Test
@@ -61,7 +64,9 @@ public final class VolunteerOpportunityTest {
     skills.add("Playing an instrument");
     skills.add("Performing");
     VolunteeringOpportunity opportunityWithSkills =
-        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS).setRequiredSkills(skills).build();
+        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS)
+            .setRequiredSkills(skills)
+            .build();
 
     VolunteeringOpportunity changedOpportunity =
         opportunityWithSkills.toBuilder().addRequiredSkill(DANCING).build();
@@ -74,11 +79,13 @@ public final class VolunteerOpportunityTest {
 
   @Test
   public void removeSkillsWithBuild() {
-     Set<String> skills = new HashSet<String>();
+    Set<String> skills = new HashSet<String>();
     skills.add("Playing an instrument");
     skills.add("Performing");
     VolunteeringOpportunity opportunityWithSkills =
-        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS).setRequiredSkills(skills).build();
+        new VolunteeringOpportunity.Builder(NAME, NUMBER_OF_SPOTS)
+            .setRequiredSkills(skills)
+            .build();
     VolunteeringOpportunity changedOpportunity =
         opportunityWithSkills.toBuilder().removeRequiredSkill(DANCING).build();
 
