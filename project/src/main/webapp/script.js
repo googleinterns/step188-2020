db0a0e7c7ad3b7893e4dbfcb8a35ff97f3edc593
nikhitashanker <rocketns@gmail.com>
2020-07-27 23:10:20 +0000

    Change what is visible based on login status.

---
+++ project/src/main/webapp/script.js
@@ -0,0 +1,59 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+window.onload = function onLoad() {
+  checkLoginStatus();
+};
+
+/*
+ * Checks login status and shows comment form, and logout prompt if
+ * user is logged in, and shows login prompt if user is not logged in.
+ */
+function checkLoginStatus() {
+  fetch('/login-status')
+      .then((response) => {
+        return response.json();
+      })
+      .then((loginStatus) => {
+        fetch('/logout-url')
+            .then((response) => {
+              return response.text();
+            })
+            .then((logoutUrl) => {
+              const logoutPrompt = document.getElementById('logout-prompt');
+              logoutPrompt.href = logoutUrl;
+            });
+        const isLoggedIn = loginStatus.isLoggedIn;
+        if (isLoggedIn) {
+          fetchLogoutUrl();
+          $('#header').load('header.html', function() {
+            $('.active').removeClass('active');
+            const currentPageArr = window.location.href.split(/[/|.]/);
+            const currentPage = currentPageArr[currentPageArr.length - 2];
+            if (currentPage === 'profile') {
+              $('#profile-header').addClass('active');
+            } else if (currentPage === 'events-feed') {
+              $('#feed-header').addClass('active');
+            } else {
+              $('#home-header').addClass('active');
+            }
+          });
+
+        } else {
+          location.href = 'login.html';
+        }
+      });
+}
+
+function fetchLogoutUrl() {}
