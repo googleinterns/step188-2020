bc64cb80221d501df04909d5900c6cd18656ddce
Stephanie Fu <fus@mit.edu>
2020-07-28 04:24:49 +0000

    Add working test code

--- project/src/main/java/com/google/sps/utilities/DatabaseWrapper.java
+++ project/src/main/java/com/google/sps/utilities/DatabaseWrapper.java
@@ -10,45 +10,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DatabaseWrapper {
-  // private static String INSTANCE_ID;
-  // private static String DATABASE_ID;
+  private static String INSTANCE_ID;
+  private static String DATABASE_ID;
 
-  // public DatabaseWrapper(String instanceID, String databaseID) {
-  //   this.INSTANCE_ID = instanceID;
-  //   this.DATABASE_ID = databaseID;
-  // }
+  public DatabaseWrapper(String instanceID, String databaseID) {
+    this.INSTANCE_ID = instanceID;
+    this.DATABASE_ID = databaseID;
+  }
 
-  // public static void insertUsers(List<User> users) {
-  //   SpannerOptions options = SpannerOptions.newBuilder().build();
-  //   Spanner spanner = options.getService();
-  //   DatabaseId db = DatabaseId.of(options.getProjectId(), INSTANCE_ID, DATABASE_ID);
-  //   DatabaseClient dbClient = spanner.getDatabaseClient(db);
+  public static void insertUsers(List<User> users) {
+    SpannerOptions options = SpannerOptions.newBuilder().build();
+    Spanner spanner = options.getService();
+    DatabaseId db = DatabaseId.of(options.getProjectId(), INSTANCE_ID, DATABASE_ID);
+    DatabaseClient dbClient = spanner.getDatabaseClient(db);
 
-  //   List<Mutation> mutations = new ArrayList<>();
-  //   for (User user : users) {
-  //     mutations.add(
-  //         Mutation.newInsertBuilder("Users")
-  //             .set("UserID")
-  //             .to(user.getUserId())
-  //             .set("Name")
-  //             .to(user.getName())
-  //             .set("Email")
-  //             .to(user.getEmail())
-  //             .set("Interests")
-  //             .toStringArray(user.getInterests())
-  //             .set("Skills")
-  //             .toStringArray(user.getSkills())
-  //             .set("EventsHosting")
-  //             .toInt64Array(user.getEventsHostingIDs())
-  //             .set("EventsParticipating")
-  //             .toInt64Array(user.getEventsParticipatingIDs())
-  //             .set("EventsVolunteering")
-  //             .toInt64Array(user.getEventsVolunteeringIDs())
-  //             .build());
-  //   }
-  //   dbClient.write(mutations);
-  //   spanner.close();
-  // }
+    List<Mutation> mutations = new ArrayList<>();
+    for (User user : users) {
+      mutations.add(
+          Mutation.newInsertBuilder("Users")
+              .set("UserID")
+              .to(user.getUserId())
+              .set("Name")
+              .to(user.getName())
+              .set("Email")
+              .to(user.getEmail())
+              .set("Interests")
+              .toStringArray(user.getInterests())
+              .set("Skills")
+              .toStringArray(user.getSkills())
+              .set("EventsHosting")
+              .toInt64Array(user.getEventsHostingIDs())
+              .set("EventsParticipating")
+              .toInt64Array(user.getEventsParticipatingIDs())
+              .set("EventsVolunteering")
+              .toInt64Array(user.getEventsVolunteeringIDs())
+              .build());
+    }
+    dbClient.write(mutations);
+    spanner.close();
+  }
 
   public static String createRestoredSampleDbId(DatabaseId database) {
     int index = database.getDatabase().indexOf('-');
