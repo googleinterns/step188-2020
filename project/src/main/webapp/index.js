3f62febc6a11d73977c70acf1610b9420ed83636
nikhitashanker <rocketns@gmail.com>
2020-07-28 00:16:27 +0000

    Add login to landing page.

---
+++ project/src/main/webapp/index.js
@@ -0,0 +1,14 @@
+window.onload = function onLoad() {
+  fetchLoginUrl();
+};
+
+function fetchLoginUrl() {
+  fetch('/login-url')
+      .then((response) => {
+        return response.text();
+      })
+      .then((loginUrl) => {
+        const loginPrompt = document.getElementById('login-prompt');
+        loginPrompt.href = loginUrl;
+      });
+}
