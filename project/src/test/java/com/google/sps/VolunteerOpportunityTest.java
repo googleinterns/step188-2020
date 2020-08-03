b59021cd9a76c572d2bf60cfe884f07b3ed343bc
nikhitashanker <rocketns@gmail.com>
2020-08-03 16:34:42 +0000

    Fixes from code review.

--- project/src/test/java/com/google/sps/VolunteerOpportunityTest.java
+++ project/src/test/java/com/google/sps/VolunteerOpportunityTest.java
@@ -12,7 +12,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/** */
 @RunWith(JUnit4.class)
 public final class VolunteerOpportunityTest {
   private static final String NAME = "Performer";
