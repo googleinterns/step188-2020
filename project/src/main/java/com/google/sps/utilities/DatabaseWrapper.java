d1e58cef03e6153e6f5360abcace933ba9eed56f
nikhitashanker <rocketns@gmail.com>
2020-08-03 17:59:50 +0000

    Make naming consistent.

--- project/src/main/java/com/google/sps/utilities/DatabaseWrapper.java
+++ project/src/main/java/com/google/sps/utilities/DatabaseWrapper.java
@@ -154,11 +154,11 @@ public class DatabaseWrapper {
       while (resultSet.next()) {
         String opportunityId = resultSet.getString(0);
         String name = resultSet.getString(1);
-        long numberOfSpots = resultSet.getLong(2);
+        long numSpotsLeft = resultSet.getLong(2);
         Set<String> requiredSkills =
             resultSet.getStringList(3).stream().collect(Collectors.toSet());
         results.add(
-            new VolunteeringOpportunity.Builder(eventId, name, numberOfSpots)
+            new VolunteeringOpportunity.Builder(eventId, name, numSpotsLeft)
                 .setOpportunityId(opportunityId)
                 .setRequiredSkills(requiredSkills)
                 .build());
