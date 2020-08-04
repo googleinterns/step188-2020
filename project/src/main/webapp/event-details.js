e022ec5b9640833afa59ccd3a33bb5fc2abfceb6
nikhitashanker <rocketns@gmail.com>
2020-08-04 18:01:30 +0000

    Add required skills to display.

--- project/src/main/webapp/event-details.js
+++ project/src/main/webapp/event-details.js
@@ -3,21 +3,29 @@ window.onload = function onLoad() {
 };
 
 async function getVolunteeringOpportunities() {
-    const response = await fetch('/event-volunteering-data');
-    const opportunities = await response.json()
-     console.log(opportunities);
-    const opportunitiesArray = JSON.parse(opportunities);
-   
-    opportunitiesArray.forEach(function(opportunity) {
-    console.log(opportunity);
-        $('#volunteering-opportunities').append(getInputFieldForOpportunity(opportunity.name, opportunity.numSpotsLeft))
-    });
+  const response = await fetch('/event-volunteering-data');
+  const opportunities = await response.json()
+  for (const key in opportunities) {
+    console.log(opportunities[key].name);
+    console.log(opportunities[key].numSpotsLeft);
+    $('#volunteering-opportunities')
+        .append(getInputFieldForOpportunity(
+            opportunities[key].name, opportunities[key].numSpotsLeft,
+            opportunities[key].requiredSkills));
+  }
 }
+
 /**
- * Return input string for skill input field.
+ * Return a list field representing the
+ * @param name name of opportunity
+ * @param numSpotsLeft number of spots left for opportunity
+ * @param requiredSkills required skills for opportunity
  * @return {string}
  */
-function getInputFieldForSkill(name, numSpotsLeft) {
-  return `<p class="card-text">Volunteer Name: {name}</p>
-              <p class="card-text">Volunteer Spots Left: {numSpotsLeft}</p>`;
+function getInputFieldForOpportunity(name, numSpotsLeft, requiredSkills) {
+  requiredSkillsString = requiredSkills.empty() ? "None" : requiredSkills.toString();
+  return `<li class="list-group-item">
+          <p class="card-text">Volunteer Name: ${name}</p>
+           <p class="card-text">Volunteer Spots Left: ${numSpotsLeft}</p>
+           <p class="card-text">Required Skills: ${requiredSkillsString}</p></li>`;
 }
