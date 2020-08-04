0346ad3536f36f30bbbd84159af839164c81d2ca
nikhitashanker <rocketns@gmail.com>
2020-08-04 19:56:20 +0000

    Fix javadoc.

--- project/src/main/webapp/event-details.js
+++ project/src/main/webapp/event-details.js
@@ -11,7 +11,7 @@ async function getVolunteeringOpportunities() {
   for (const key in opportunities) {
     if (opportunities.hasOwnProperty(key)) {
       $('#volunteering-opportunities')
-          .append(getInputFieldForOpportunity(
+          .append(getListItemForOpportunity(
               opportunities[key].name, opportunities[key].numSpotsLeft,
               opportunities[key].requiredSkills));
     }
@@ -22,16 +22,16 @@ async function getVolunteeringOpportunities() {
  * Return a string for a list HTML element representing an opportunity.
  * @param {string} name name of opportunity
  * @param {string} numSpotsLeft number of spots left for opportunity
- * @param {string[]} required skills for opportunity
+ * @param {string[]} requiredSkills skills for opportunity
  * @return {string}
  */
-function getInputFieldForOpportunity(name, numSpotsLeft, requiredSkills) {
+function getListItemForOpportunity(name, numSpotsLeft, requiredSkills) {
   requiredSkillsText =
       requiredSkills.length ? requiredSkills.toString() : 'None';
   return `<li class="list-group-item">
           <p class="card-text">Volunteer Name: ${name}</p>
            <p class="card-text">Volunteer Spots Left: ${numSpotsLeft}</p>
            <p class="card-text">Required Skills: ${
-      requiredSkillsText}</p></li>`;
+  requiredSkillsText}</p></li>`;
 }
 
