ada33c22328962839520b0ae3b9842c7aeb79a81
Yichen <yicyao@gmail.com>
2020-08-04 18:54:10 +0000

    link to editable event page

--- project/src/main/webapp/event-details.js
+++ project/src/main/webapp/event-details.js
@@ -1,15 +1,22 @@
-// Gets event details from database and fills out event page with details
+/**
+ * Gets event details from database and fills out event page with details
+ */
 function getEventDetails() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
+  const eventId = urlParams.get('eventId');
   fetch(
       '/create-event?' +
-      new URLSearchParams({'eventId': urlParams.get('eventId')}))
-      .then(res => res.json())
-      .then(data => {
+      new URLSearchParams({'eventId': eventId}))
+      .then((res) => (res.json())).then((data) => {
         document.getElementById('name').innerHTML = data['name'];
         document.getElementById('description').innerHTML = data['description'];
-        document.getElementById('date').innerHTML = `Date: ${data['date'].month}/${data['date'].dayOfMonth}/${data['date'].year}`;
-        document.getElementById('location').innerHTML  = `Location: ${data['location']}`;
+        document.getElementById('date').innerHTML = `Date: 
+          ${data['date'].month}/${data['date'].dayOfMonth}/${data['date'].year}`;
+        document.getElementById('location').innerHTML =
+          `Location: ${data['location']}`;
+        
+        const link = '/event-edit.html?eventId=' + eventId
+        document.getElementById('editLink').setAttribute("href", link);
       });
 }
