f1ba7ac23a369ee39d391af83d403284b77489da
Stephanie Fu <fus@mit.edu>
2020-08-04 16:47:04 +0000

    Clean up frontend profile editing

--- project/src/main/webapp/profile-edit.js
+++ project/src/main/webapp/profile-edit.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-  fetch('/profile-update')
-      .then((response) => response.json())
-      .then((userData) => {
-        const userName = userData['name'];
-        const userInterests = userData['interests'];
-        const userSkills = userData['skills'];
-
-        $('#name').val(userName);
-        $('#interests').val(userInterests);
-        $('#skills').val(userSkills);
-      });
+  showCurrentUserInfo();
 });
+
+async function showCurrentUserInfo() {
+  const response = await fetch('/profile-update');
+  const userData = await response.json();
+
+  const userName = userData['name'];
+  const userInterests = userData['interests'];
+  const userSkills = userData['skills'];
+
+  $('#name').val(userName);
+  $('#interests').val(userInterests);
+  $('#skills').val(userSkills);
+}
