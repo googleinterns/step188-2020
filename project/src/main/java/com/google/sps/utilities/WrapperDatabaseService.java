1db59821440c0a1ea295174973d0f45d78ba4359
nikhitashanker <rocketns@gmail.com>
2020-08-06 17:09:26 +0000

    Fix from code review.

--- project/src/main/java/com/google/sps/utilities/WrapperDatabaseService.java
+++ project/src/main/java/com/google/sps/utilities/WrapperDatabaseService.java
@@ -6,15 +6,14 @@ import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
 
 public class WrapperDatabaseService implements DatabaseService {
-  public static final String INSTANCE_ID = "step-188-instance";
-  public static final String DATABASE_ID = "event-organizer-db";
   private final Spanner spanner;
   private final DatabaseClient databaseClient;
 
   public WrapperDatabaseService() {
     SpannerOptions options = SpannerOptions.newBuilder().build();
     spanner = options.getService();
-    DatabaseId database = DatabaseId.of(options.getProjectId(), INSTANCE_ID, DATABASE_ID);
+    DatabaseId database =
+        DatabaseId.of(options.getProjectId(), DatabaseConstants.INSTANCE_ID, DatabaseConstants.DATABASE_ID);
     databaseClient = spanner.getDatabaseClient(database);
   }
 
