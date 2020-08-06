2d27868a9e83c8d08804c8a29f1b77ec27742341
Stephanie Fu <fus@mit.edu>
2020-08-06 17:55:27 +0000

    New EventDetailServlet

---
+++ project/src/main/java/com/google/sps/servlets/EventDetailServlet.java
@@ -0,0 +1,36 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.cloud.Date;
+import com.google.gson.Gson;
+import com.google.sps.data.Event;
+import com.google.sps.data.User;
+import com.google.sps.utilities.DatabaseConstants;
+import com.google.sps.utilities.DatabaseWrapper;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/event-details")
+public class EventDetailServlet extends HttpServlet {
+  private static final DatabaseWrapper dbWrapper = new DatabaseWrapper(DatabaseConstants.INSTANCE_ID, DatabaseConstants.DATABASE_ID);
+
+  /** Returns all event details from database */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    List<Event> events = dbWrapper.getAllEvents();
+    response.setContentType("text/html;");
+    response.getWriter().println(new Gson().toJson(events));
+  }
+}
