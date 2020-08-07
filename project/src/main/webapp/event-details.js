7c8d671ba7b40369d17875d0be6ef89c24f6723e
Yichen <yicyao@gmail.com>
2020-08-07 18:21:18 +0000

    change to async/await

--- project/src/main/webapp/event-details.js
+++ project/src/main/webapp/event-details.js
@@ -82,23 +82,28 @@ function getLinkForOpportunity(opportunityId) {
 }
 
 /**
- * Gets event details from database and fills out event page with details
+ * Gets event details from database with eventId and fills out event page with details
  */
-function getEventDetails() {
+async function getEventDetails() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const eventId = urlParams.get('eventId');
-  fetch(
-      '/create-event?' +
-      new URLSearchParams({'eventId': eventId}))
-      .then((res) => (res.json())).then((data) => {
-        document.getElementById('name').innerHTML = data['name'];
-        document.getElementById('description').innerHTML = data['description'];
-        document.getElementById('date').innerHTML = `Date: 
-        ${data['date'].month}/${data['date'].dayOfMonth}/${data['date'].year}`;
-        document.getElementById('location').innerHTML =
-          `Location: ${data['location']}`;
-        const link = '/event-edit.html?eventId=' + eventId;
-        document.getElementById('editLink').setAttribute('href', link);
-      });
+  const response = await fetch('/create-event?' + new URLSearchParams({'eventId': eventId}))
+  const data = await response.json();
+//   fetch(
+//       '/create-event?' +
+//       new URLSearchParams({'eventId': eventId}))
+//       .then((res) => (res.json())).then((data) => {
+  document.getElementById('name').innerHTML = data['name'];
+  document.getElementById('description').innerHTML = data['description'];
+  document.getElementById('date').innerHTML = `Date: 
+  ${data['date'].month}/${data['date'].dayOfMonth}/${data['date'].year}`;
+  document.getElementById('location').innerHTML =
+    `Location: ${data['location']}`;
+  const link = '/event-edit.html?eventId=' + eventId;
+  document.getElementById('editLink').setAttribute('href', link);
+    //   });
 }
+
+//   const response = await fetch('/login-status');
+//   const loginStatus = await response.json();
