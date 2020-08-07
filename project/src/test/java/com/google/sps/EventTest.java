98aef872e5e023c1150bed5a3f6d85537a895d17
Yichen <yicyao@gmail.com>
2020-08-07 18:32:39 +0000

    linter

--- project/src/test/java/com/google/sps/EventTest.java
+++ project/src/test/java/com/google/sps/EventTest.java
@@ -21,25 +21,35 @@ public final class EventTest {
   private static final String EVENT_NAME = "Team Meeting";
   private static final String NEW_EVENT_NAME = "Daily Team Meeting";
   private static final String DESCRIPTION = "Daily Team Sync";
-  private static final Set <String> LABELS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList("Tech", "Work")));
+  private static final Set <String> LABELS =
+      Collections.unmodifiableSet(new HashSet<>(Arrays.asList("Tech", "Work")));
   private static final String LOCATION = "Remote";
   private static final Date DATE = Date.fromYearMonthDay(2016, 9, 15);
   private static final String TIME = "3:00PM-5:00PM";
   private static final String VOLUNTEER_OPPORTUNITY_NAME = "Volunteer";
   private static final int NUM_SPOTS_LEFT = 5;
   private static final String EVENT_ID = "0883de79-17d7-49a3-a866-dbd5135062a8";
-  private static final VolunteeringOpportunity OPPORTUNITY = new VolunteeringOpportunity.Builder(EVENT_ID, VOLUNTEER_OPPORTUNITY_NAME, NUM_SPOTS_LEFT).build();
-  private static final Set <VolunteeringOpportunity> OPPORTUNITIES = new HashSet <VolunteeringOpportunity> (Arrays.asList(OPPORTUNITY));
+  private static final VolunteeringOpportunity OPPORTUNITY =
+      new VolunteeringOpportunity.Builder(EVENT_ID, VOLUNTEER_OPPORTUNITY_NAME, NUM_SPOTS_LEFT)
+          .build();
+  private static final Set <VolunteeringOpportunity> OPPORTUNITIES =
+      new HashSet <VolunteeringOpportunity> (Arrays.asList(OPPORTUNITY));
   private static final User USER1 = new User.Builder("USER1", "USER1@test.com").build();
   private static final User USER2 = new User.Builder("USER2", "USER2@test.com").build();
   private static final User USER3 = new User.Builder("USER3", "USER3@test.com").build();
-  private static final Set <User> ATTENDEES = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(USER1)));
+  private static final Set <User> ATTENDEES =
+      Collections.unmodifiableSet(new HashSet<>(Arrays.asList(USER1)));
 
-  /** Create an event and verify Builder class is created with correct required and optional fields */
+  /**
+   * Create an event and verify Builder class is created with correct required and optional fields
+   */
   @Test
   public void testEventBuild() {
-    Event event = new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST)
-      .setOpportunities(OPPORTUNITIES).setAttendees(ATTENDEES).build();
+    Event event =
+        new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST)
+            .setOpportunities(OPPORTUNITIES)
+            .setAttendees(ATTENDEES)
+            .build();
 
     Assert.assertEquals(event.getName(), EVENT_NAME);
     Assert.assertEquals(event.getDescription(), DESCRIPTION);
@@ -55,7 +65,10 @@ public final class EventTest {
   /** Edit an event and verify Event mergeFrom setter sets required and optional field */
   @Test
   public void setEventFields() {
-    Event event = new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST).setAttendees(ATTENDEES).build();
+    Event event =
+        new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST)
+            .setAttendees(ATTENDEES)
+            .build();
     Set <User> user2Attendees = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(USER2)));
     event = event.toBuilder().setName(NEW_EVENT_NAME).setAttendees(user2Attendees).build();
 
@@ -66,10 +79,13 @@ public final class EventTest {
   /** Add attendees and verify event adding attendees */
   @Test
   public void addAttendeeField() {
-    Event event = new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST).setAttendees(
-      new HashSet<User>(Arrays.asList(USER1, USER2))).build();
+    Event event =
+        new Event.Builder(EVENT_NAME, DESCRIPTION, LABELS, LOCATION, DATE, TIME, HOST)
+            .setAttendees(new HashSet<User>(Arrays.asList(USER1, USER2)))
+            .build();
     event = event.toBuilder().addAttendee(USER3).build();
 
-    Assert.assertEquals(event.getAttendees(), new HashSet<User>(Arrays.asList(USER1, USER2, USER3)));
+    Assert.assertEquals(
+        event.getAttendees(), new HashSet<User>(Arrays.asList(USER1, USER2, USER3)));
   }
 }
