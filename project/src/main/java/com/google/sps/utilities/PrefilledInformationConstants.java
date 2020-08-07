bcac7d2d1df8c5c6f53df95092c143551d7a1fab
Stephanie Fu <fus@mit.edu>
2020-08-07 04:02:22 +0000

    Add new lines

--- project/src/main/java/com/google/sps/utilities/PrefilledInformationConstants.java
+++ project/src/main/java/com/google/sps/utilities/PrefilledInformationConstants.java
@@ -6,41 +6,41 @@ import java.util.List;
 
 public final class PrefilledInformationConstants {
   public static final List<String> interests = Arrays.asList(
-    "american football", "archery", "art", "badminton", "baseball", "baking", 
-    "basketball", "beach volleyball" "bicycling", "billiards", "bodybuilding", 
-    "bowling", "boxing", "cheerleading", "chess", "competitions", 
-    "cooking", "cricket", "curling", "dancing", "darts", "debate", "disc golf", 
-    "dodgeball", "fantasy sports", "flag football", "food", "golfing", "handball", 
-    "horseback riding", "ice hockey", "lacrosse", "paintball", "roller skating", 
-    "rugby", "sailing", "skateboarding", "skiing", "snowboarding", "soccer", 
-    "surfing", "swimming", "table tennis", "tennis", "ultimate frisbee", 
-    "volleyball", "wrestling", "calligraphy", "cartooning", "cards", "ceramics", 
-    "coloring", "crocheting", "cross-stitch", "music", "drawing", "embroidery", 
-    "fashion", "knitting", "origami", "painting", "photography", "pottery", 
-    "cartoons", "poetry", "bird-watching", "gaming", "socializing", "coffee",
-    "sewing", "singing", "sketching", "taxidermy", "textiles", "crosswords", 
-    "dolls", "electronics", "history", "jigsaw puzzles", "laser tag", "magic", 
-    "mahjong", "meditation", "gymnastics", "robotics", "sudoku", "tetris", "TV", 
-    "writing", "grilling", "bbq", "brewing", "cake decorating", "kombucha", 
+    "american football", "archery", "art", "badminton", "baseball", "baking", \
+    "basketball", "beach volleyball" "bicycling", "billiards", "bodybuilding", \
+    "bowling", "boxing", "cheerleading", "chess", "competitions", \
+    "cooking", "cricket", "curling", "dancing", "darts", "debate", "disc golf", \
+    "dodgeball", "fantasy sports", "flag football", "food", "golfing", "handball", \
+    "horseback riding", "ice hockey", "lacrosse", "paintball", "roller skating", \
+    "rugby", "sailing", "skateboarding", "skiing", "snowboarding", "soccer", \
+    "surfing", "swimming", "table tennis", "tennis", "ultimate frisbee", \
+    "volleyball", "wrestling", "calligraphy", "cartooning", "cards", "ceramics", \
+    "coloring", "crocheting", "cross-stitch", "music", "drawing", "embroidery", \
+    "fashion", "knitting", "origami", "painting", "photography", "pottery", \
+    "cartoons", "poetry", "bird-watching", "gaming", "socializing", "coffee", \
+    "sewing", "singing", "sketching", "taxidermy", "textiles", "crosswords", \
+    "dolls", "electronics", "history", "jigsaw puzzles", "laser tag", "magic", \
+    "mahjong", "meditation", "gymnastics", "robotics", "sudoku", "tetris", "TV", \
+    "writing", "grilling", "bbq", "brewing", "cake decorating", "kombucha", \
     "winemaking", "canning"
   );
 
   public static final List<String> skills = Arrays.asList(
-    "american football", "archery", "badminton", "baseball", "baking", 
-    "basketball", "beach volleyball" "bicycling", "billiards", "bodybuilding", 
-    "bowling", "boxing", "cheerleading", "chess", "competitions", 
-    "cooking", "cricket", "curling", "dancing", "darts", "debate", "disc golf", 
-    "dodgeball", "fantasy sports", "flag football", "golfing", "handball", 
-    "horseback riding", "ice hockey", "lacrosse", "paintball", "roller skating", 
-    "rugby", "sailing", "skateboarding", "skiing", "snowboarding", "soccer", 
-    "surfing", "swimming", "table tennis", "tennis", "ultimate frisbee", 
-    "volleyball", "wrestling", "calligraphy", "cartooning", "cards", "ceramics", 
-    "coloring", "crocheting", "cross-stitch", "drawing", "embroidery", 
-    "fashion", "knitting", "origami", "painting", "photography", "pottery", 
-    "sewing", "singing", "sketching", "taxidermy", "textiles", "crosswords", 
-    "dolls", "electronics", "history", "jigsaw puzzles", "laser tag", "magic", 
-    "mahjong", "meditation", "gymnastics", "robotics", "sudoku", "tetris", "TV", 
-    "writing", "grilling", "bbq", "brewing", "cake decorating", "kombucha", 
+    "american football", "archery", "badminton", "baseball", "baking", \
+    "basketball", "beach volleyball" "bicycling", "billiards", "bodybuilding", \
+    "bowling", "boxing", "cheerleading", "chess", "competitions", \
+    "cooking", "cricket", "curling", "dancing", "darts", "debate", "disc golf", \
+    "dodgeball", "fantasy sports", "flag football", "golfing", "handball", \
+    "horseback riding", "ice hockey", "lacrosse", "paintball", "roller skating", \
+    "rugby", "sailing", "skateboarding", "skiing", "snowboarding", "soccer", \
+    "surfing", "swimming", "table tennis", "tennis", "ultimate frisbee", \
+    "volleyball", "wrestling", "calligraphy", "cartooning", "cards", "ceramics", \
+    "coloring", "crocheting", "cross-stitch", "drawing", "embroidery", \
+    "fashion", "knitting", "origami", "painting", "photography", "pottery", \
+    "sewing", "singing", "sketching", "taxidermy", "textiles", "crosswords", \
+    "dolls", "electronics", "history", "jigsaw puzzles", "laser tag", "magic", \
+    "mahjong", "meditation", "gymnastics", "robotics", "sudoku", "tetris", "TV", \
+    "writing", "grilling", "bbq", "brewing", "cake decorating", "kombucha", \
     "winemaking", "canning"
   );
 }
