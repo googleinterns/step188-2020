8f719ace6e20e77c350500264c9afa1057eae3dd
Stephanie Fu <fus@mit.edu>
2020-08-10 22:22:31 +0000

    Revert event creation fix

--- project/src/main/java/com/google/sps/servlets/EventCreationServlet.java
+++ project/src/main/java/com/google/sps/servlets/EventCreationServlet.java
@@ -30,12 +30,12 @@ public class EventCreationServlet extends HttpServlet {
     Optional<Event> eventOptional = SpannerTasks.getEventById(eventId);
 
     // If event DNE, sends 404 ERR to frontend
-    if (eventOptional.isPresent()) {
+    if (!eventOptional.isPresent()) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    } else {
       Event event = eventOptional.get();
       response.setContentType("text/html;");
       response.getWriter().println(new Gson().toJson(event));
-    } else {
-      response.sendError(HttpServletResponse.SC_NOT_FOUND);
     }  
   }
 
