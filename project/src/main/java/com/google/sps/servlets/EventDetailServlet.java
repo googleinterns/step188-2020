083d477e305e0813330f46d81e3c25ad7a1ff5ab
Stephanie Fu <fus@mit.edu>
2020-08-11 01:52:22 +0000

    Modify EventDetails unit tests

--- project/src/main/java/com/google/sps/servlets/EventDetailServlet.java
+++ project/src/main/java/com/google/sps/servlets/EventDetailServlet.java
@@ -1,20 +1,9 @@
 package com.google.sps.servlets;
 
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.cloud.Date;
-import com.google.gson.Gson;
 import com.google.sps.data.Event;
-import com.google.sps.data.User;
+import com.google.sps.utilities.CommonUtils;
 import com.google.sps.utilities.SpannerTasks;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -28,6 +17,6 @@ public class EventDetailServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     Set<Event> events = SpannerTasks.getAllEvents();
     response.setContentType("application/json;");
-    response.getWriter().println(new Gson().toJson(events));
+    response.getWriter().println(CommonUtils.convertToJson(events));
   }
 }
