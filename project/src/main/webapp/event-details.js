1a3b409bb4b971237cb275e3e9e2fb0d45740b28
Yichen <yicyao@gmail.com>
2020-08-13 18:42:11 +0000

    do not revert

--- project/src/main/webapp/event-details.js
+++ project/src/main/webapp/event-details.js
@@ -6,7 +6,6 @@ window.onload = function onLoad() {
   getEventDetails();
   checkLoginStatus();
   getVolunteeringOpportunities();
-  populateOpportunitiesDropdown();
 };
 
 
@@ -29,15 +28,12 @@ async function getVolunteeringOpportunities() {
   const opportunities = await response.json();
   for (const key in opportunities) {
     if (opportunities.hasOwnProperty(key)) {
-      const volunteers =
-          await getVolunteersByOpportunityId(opportunities[key].opportunityId);
       $('#volunteering-opportunities')
           .append(getListItemForOpportunity(
               opportunities[key].opportunityId,
               opportunities[key].name,
               opportunities[key].numSpotsLeft,
-              opportunities[key].requiredSkills,
-              volunteers));
+              opportunities[key].requiredSkills));
     }
   }
 }
@@ -51,15 +47,12 @@ async function getVolunteeringOpportunities() {
  * @param {string} name name of opportunity
  * @param {string} numSpotsLeft number of spots left for opportunity
  * @param {string[]} requiredSkills skills for opportunity
- * @param {string} volunteers volunteers for opportunity
  * @return {string}
  */
 function getListItemForOpportunity(
-    opportunityId, name, numSpotsLeft, requiredSkills, volunteers) {
+    opportunityId, name, numSpotsLeft, requiredSkills) {
   requiredSkillsText =
       requiredSkills.length ? requiredSkills.toString() : 'None';
-  volunteersText =
-      volunteers.length ? volunteers.toString() : 'None';
   let editLink = '';
 
   // If the user is logged in and the current user is the event host,
@@ -70,8 +63,7 @@ function getListItemForOpportunity(
   return `<li class="list-group-item">
           <p class="card-text">Volunteer Name: ${name}</p>
            <p class="card-text">Volunteer Spots Left: ${numSpotsLeft}</p>
-           <p class="card-text">Required Skills: ${requiredSkillsText}</p>
-           <p class="card-text">Volunteers: ${volunteersText}</p>${
+           <p class="card-text">Required Skills: ${requiredSkillsText}</p>${
   editLink}</li>`;
 }
 
@@ -91,15 +83,25 @@ function getLinkForOpportunity(opportunityId) {
 }
 
 /**
- * Gets event details from database with eventId and uses to fill event page
+ * Gets event details from database with eventId and fills out event page with details
+ * If registering for event, register user then show event details
  */
 async function getEventDetails() {
+    //make sign up link go to correct
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const eventId = urlParams.get('eventId');
-  const response = await fetch('/create-event?' +
-    new URLSearchParams({'eventId': eventId}));
+
+  //Register for event, if register param not specified, default to not registering
+  const registerBool = urlParams.get('register') ? urlParams.get('register'): 'false';
+  if (registerBool === "true") {
+      registerEvent(eventId)
+  }
+
+  //View event details
+  const response = await fetch('/create-event?' + new URLSearchParams({'eventId': eventId}))
   const data = await response.json();
+  console.log(data);
   document.getElementById('name').innerHTML = data['name'];
   document.getElementById('description').innerHTML = data['description'];
   document.getElementById('date').innerHTML = `Date: 
@@ -107,55 +109,11 @@ async function getEventDetails() {
   document.getElementById('location').innerHTML =
     `Location: ${data['location']}`;
   document.getElementById('time').innerHTML = `Time: ${data['time']}`;
-  document.getElementById('editLink')
-    .setAttribute('href', `/event-edit.html?eventId=${eventId}`);
+  document.getElementById('editLink').setAttribute('href', `/event-edit.html?eventId=${eventId}`);
 }
 
-/**
- * Adds the volunteering opportunities for which the number of attendee spots
- * is greater than 0 to the dropdown selection.
- */
-async function populateOpportunitiesDropdown() {
-  const response = await fetch('/event-volunteering-data');
-  const opportunities = await response.json();
-  for (const key in opportunities) {
-    if (opportunities.hasOwnProperty(key)) {
-      if (opportunities[key].numSpotsLeft > 0) {
-        $('#opportunities-options')
-            .append(getOptionForOpportunity(
-                opportunities[key].opportunityId,
-                opportunities[key].name));
-      }
-    }
-  }
-}
-
-/**
- * Returns option with given name as text and opportunityId as value.
- * @param {string} opportunityId value of the opportunity option to
- *     return
- * @param {string} name text for the opportunity option to return
- * @return {string}
- */
-function getOptionForOpportunity(opportunityId, name) {
-  return `<option value=${opportunityId}>${name}</option>`;
-}
-
-/**
- * Returns volunteer emails for the given opportunityId.
- * @param {string} opportunityId Opportunity ID for which to return
- *     volunteer emails
- * @return {string[]}
- */
-async function getVolunteersByOpportunityId(opportunityId) {
-  const response =
-      await fetch(`/opportunity-signup-data?opportunity-id=${opportunityId}`);
-  const volunteerData = await response.json();
-  const volunteers = [];
-  for (const key in volunteerData) {
-    if (volunteerData.hasOwnProperty(key)) {
-      volunteers.push(volunteerData[key].email);
-    }
-  }
-  return volunteers;
+async function registerEvent(eventId) {
+  const response = await fetch('/register-event?' + new URLSearchParams({'eventId': eventId}), {method: 'POST'} );
+  document.getElementById('signupLink').setAttribute('href', '');
+  document.getElementById('signupLink').innerHTML = 'You are signed up for this event.'
 }
