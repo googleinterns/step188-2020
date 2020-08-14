fbfff96a604565885b6655f8ea728fe36eebdeeb
nikhitashanker <rocketns@gmail.com>
2020-08-14 22:22:45 +0000

    Add event query parameter when creating an opportunity.

--- project/src/main/webapp/create-volunteering-opportunity.js
+++ project/src/main/webapp/create-volunteering-opportunity.js
@@ -1,4 +1,18 @@
 $(function() {
   populatePrefilled('interests');
   populatePrefilled('skills');
+  setActionToCorrectRedirectURL();
 });
+
+/**
+ * Set the form action to include query parameter for eventId
+ */
+async function setActionToCorrectRedirectURL() {
+  const params = (new URL(document.location)).searchParams;
+  const eventId = params.get('event-id');
+
+  const opportunityForm = document.getElementById('volunteering-opportunity-form');
+  opportunityForm.action =
+      `/volunteering-form-handler?event-id=${eventId}`;
+}
+
