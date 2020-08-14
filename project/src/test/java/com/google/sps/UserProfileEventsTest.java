76603568014cf43eb4a3983021aea1e25f832995
Yichen <yicyao@gmail.com>
2020-08-14 02:24:38 +0000

    test

--- project/src/test/java/com/google/sps/UserProfileEventsTest.java
+++ project/src/test/java/com/google/sps/UserProfileEventsTest.java
@@ -29,6 +29,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.mockito.Mockito;
 import org.springframework.mock.web.MockServletContext;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.json.JSONException;
 
 /** Unit tests for updates and retrievals for a user's corresponding events. */
 @RunWith(JUnit4.class)
@@ -42,8 +44,9 @@ public class UserProfileEventsTest {
       new LocalServiceTestHelper(new LocalUserServiceTestConfig());
   private static final String PARAMETER_EVENT_TYPE = "event-type";
   private static final String VOLUNTEERING = "volunteering";
-  private static final String HOST_NAME = "Bob Smith";
-  private static final String HOST_EMAIL = "bobsmith@example.com";
+  private static final String PARTICIPATING = "participating";
+  private static final String HOST_NAME = "Host Bob";
+  private static final String HOST_EMAIL = "hostbobsmith@example.com";
   private static final String INVALID_EVENT_TYPE = "all";
   private static final String EMAIL = "test@example.com";
    private static final String AUTH_DOMAIN = "example.com";
@@ -74,11 +77,6 @@ public class UserProfileEventsTest {
   @Test
   public void getUserEventsHosting() throws IOException {
     // TO DO: Add tests for events hosting.
-  }
-
-  @Test
-  public void getUserEventsParticipating() throws IOException {
-    // TO DO: Add tests for events
   }
 
   @Test
@@ -179,4 +177,39 @@ public class UserProfileEventsTest {
     Mockito.verify(response).sendRedirect("/index.html");
   }
 
+  /*Test to get all events logged in user is participating in */
+  @Test
+  public void getUserEventsParticipating() throws IOException {
+    //Logged in user
+    User user = TestUtils.newUserWithEmail(EMAIL);
+    String userEmail = user.getEmail(); 
+    SpannerTasks.insertOrUpdateUser(user);
+    authenticationHelper
+        .setEnvIsLoggedIn(true)
+        .setEnvEmail(userEmail)
+        .setEnvAuthDomain("example.com");
+    //Random Attendee
+    User ATTENDEE = new User.Builder("New attendee", "attendee@example.com").build();
+    SpannerTasks.insertOrUpdateUser(ATTENDEE);   
+    User host = new User.Builder(HOST_NAME, HOST_EMAIL).build();
+    SpannerTasks.insertOrUpdateUser(host);
+
+    Event event = TestUtils.newEventWithHost(host).toBuilder()
+      .setAttendees(new HashSet<User>(Arrays.asList(ATTENDEE, user))).build();
+    SpannerTasks.insertorUpdateEvent(event);
+    Event event2 = TestUtils.newEventWithHost(host).toBuilder()
+      .setAttendees(new HashSet<User>(Arrays.asList(user))).build();
+    SpannerTasks.insertorUpdateEvent(event2);
+
+    Mockito.when(request.getParameter(PARAMETER_EVENT_TYPE)).thenReturn(PARTICIPATING);
+    profileEventsServlet.doGet(request, response);
+
+    try {
+    //Asserts unordered JSON, otherwise leads to flakey tests
+    JSONAssert.assertEquals(CommonUtils.convertToJson(
+        new HashSet<>(Arrays.asList(event, event2))).trim(),stringWriter.toString().trim(), false);
+    } catch (JSONException e) {
+        System.out.println("JSON conversion failed.");
+    }
+  }
 }
