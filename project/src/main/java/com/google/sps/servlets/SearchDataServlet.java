7d87a28e9a8b62b5e2d361905a887f6e3a6dbab5
nikhitashanker <rocketns@gmail.com>
2020-08-17 23:29:13 +0000

    Add error-checking to servlet.

--- project/src/main/java/com/google/sps/servlets/SearchDataServlet.java
+++ project/src/main/java/com/google/sps/servlets/SearchDataServlet.java
@@ -14,18 +14,16 @@
 
 package com.google.sps.servlets;
 
-import com.google.common.collect.ListMultimap;
 import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import com.google.sps.utilities.CommonUtils;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that handles comments data. */
+/** Servlet that handles search queries and addition of event keywords to index. */
 @WebServlet("/searchData")
 public class SearchDataServlet extends HttpServlet {
   private ListMultimap<String, String> keywordToEventIds = ArrayListMultimap.create();
@@ -36,9 +34,9 @@ public class SearchDataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String keyword = request.getParameter(KEYWORD);
-
     if (keyword == null) {
-        
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, String.format("No keyword specified."));
+      return;
     }
 
     response.setContentType("application/json;");
@@ -49,14 +47,18 @@ public class SearchDataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String eventId = request.getParameter(EVENT_ID);
     String description = CommonUtils.getParameter(request, DESCRIPTION, "");
+    if (eventId == null) {
+      response.sendError(HttpServletResponse.SC_BAD_REQUEST, String.format("No event ID specified."));
+      return;
+    }
 
     String[] keywordsInDescription = description.split("[^a-zA-Z0-9']+");
-    addWordsToIndex(eventId, keywordsInDescription);
+    addKeywordsToIndex(eventId, keywordsInDescription);
 
     response.sendRedirect("/events-feed.html");
   }
 
-  private void addWordsToIndex(String eventId, String[] keywords) {
+  private void addKeywordsToIndex(String eventId, String[] keywords) {
     for (String keyword : keywords) {
       keywordToEventIds.put(keyword, eventId);
     }
