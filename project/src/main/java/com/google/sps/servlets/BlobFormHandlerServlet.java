bd2d947a5b79e3c0224883e86222340ffde4a8a1
Stephanie Fu <fus@mit.edu>
2020-08-17 20:15:32 +0000

    Make method static

--- project/src/main/java/com/google/sps/servlets/BlobFormHandlerServlet.java
+++ project/src/main/java/com/google/sps/servlets/BlobFormHandlerServlet.java
@@ -58,7 +58,7 @@ public class BlobFormHandlerServlet extends HttpServlet {
   }
 
   /** Returns a URL that points to the uploaded file, or null if the user didn't upload a file. */
-  private String getUploadedFileUrl(HttpServletRequest request) {
+  private static String getUploadedFileUrl(HttpServletRequest request) {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
     List<BlobKey> blobKeys = blobs.get(PROFILE_PICTURE);
