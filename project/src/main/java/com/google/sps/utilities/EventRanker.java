e76740f8f2db54cf35f5c977df843fa676c4f386
Stephanie Fu <fus@mit.edu>
2020-08-18 04:57:17 +0000

    Write algorithm for ranking

--- project/src/main/java/com/google/sps/utilities/EventRanker.java
+++ project/src/main/java/com/google/sps/utilities/EventRanker.java
@@ -1,9 +1,12 @@
 package com.google.sps.utilities;
 
+import com.google.common.collect.Sets;
 import com.google.sps.data.Event;
 import com.google.sps.data.User;
 import com.google.sps.data.VolunteeringOpportunity;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,8 +14,9 @@ import java.util.Set;
 public class EventRanker {
   /**
    * Sorts events based on each event's score.
-   * Breaks ties with the event's number of VolunteeringOpportunity
-   * requiredSkills that match the user's skills.
+   * Breaks ties with the event's date; the more recent event will be
+   * ranked higher.
+   * Makes the assumption that all events are NOT in the past.
    * 
    * @param user The user for whom the events are ranked
    * @param unrankedEvents A {@link Set} of events that need to be ranked
@@ -20,10 +24,24 @@ public class EventRanker {
    * each element's score
    */
   public static List<Event> rankEvents(User user, Set<Event> unrankedEvents) {
-    return new ArrayList<>();
+    List<Event> rankedEvents = new ArrayList<>(unrankedEvents);
+    Collections.sort(rankedEvents,
+        (Event event1, Event event2) -> {
+          return getEventScore(user, event1).compareTo(getEventScore(user, event2));
+        (Event event1, Event event2) -> {
+          return event1.getDate().compareTo(event2.getDate());
+    });
+    Collections.reverse(rankedEvents);
+    return rankedEvents;
   }
 
-  private static int getEventScore(User user, Event event) {
-
+  private static Integer getEventScore(User user, Event event) {
+    Set<String> interestsAndSkills = user.getInterests();
+    interestsAndSkills.addAll(user.getSkills());
+    Set<String> labelsAndSkills = event.getLabels();
+    for (VolunteeringOpportunity opportunity : event.getOpportunities()) {
+      labelsAndSkills.addAll(opportunity.getRequiredSkills());
+    }
+    return Sets.intersection(interestsAndSkills, labelsAndSkills).size();
   }
 }
