86adb276bc65b32234f999d5f05b8a671af5e25c
Yichen <yicyao@gmail.com>
2020-08-19 20:00:25 +0000

    add new servlet

--- project/src/main/java/com/google/sps/utilities/NlpProcessing.java
+++ project/src/main/java/com/google/sps/utilities/NlpProcessing.java
@@ -13,28 +13,26 @@ import com.google.cloud.language.v1.Document.Type;
 /* Class containing NLP API Processing Results */
 public class NlpProcessing {
 
- 
 /*
  * @param text: String of text that includes event name and description
  * @return categoryNames: returns selected names of labels that NLP API suggests for text
 */
-  public static ArrayList<String> getNlp(String text) throws IOException {
+  public ArrayList<String> getNlp(String text) throws IOException {
     ArrayList<String> categoryNames = new ArrayList<String>();
 
     // Use Gcloud NLP API to predict labels based on user inputted event name and description
     try (LanguageServiceClient language = LanguageServiceClient.create()) {     
-    Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
-    ClassifyTextRequest req = ClassifyTextRequest.newBuilder().setDocument(doc).build();
-    // Detect categories in the given text
-    ClassifyTextResponse res = language.classifyText(req);
+      Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
+      ClassifyTextRequest req = ClassifyTextRequest.newBuilder().setDocument(doc).build();
+      // Detect categories in the given text
+      ClassifyTextResponse res = language.classifyText(req);
         for (ClassificationCategory category : res.getCategoriesList()) {
-            String categoryName = category.getName().split("/")[1];
+          String categoryName = category.getName().split("/")[1];
             if (category.getConfidence() >= 0.5 && PrefilledInformationConstants.INTERESTS.contains(categoryName) ) {
-                categoryNames.add(categoryName);
+              categoryNames.add(categoryName);
             }
         }
     } 
    return categoryNames;
   }
-
 }
