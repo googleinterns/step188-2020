0a47b4793ee9b9377ed7d6db06b4e305b4d31e1c
nikhitashanker <rocketns@gmail.com>
2020-08-19 02:20:11 +0000

    Add class for event result.

---
+++ project/src/main/java/com/google/sps/data/EventResult.java
@@ -0,0 +1,32 @@
+package com.google.sps.data;
+
+import java.util.Comparator;
+import java.lang.Float;
+
+/* Class that represent an event result in search. */
+public class EventResult {
+  private String eventId;
+  private float ranking;
+
+  public EventResult(String eventId, float ranking) {
+    this.eventId = eventId;
+    this.ranking = ranking;
+  }
+
+  public String getEventId() {
+    return eventId;
+  }
+
+  public float getRanking() {
+    return ranking;
+  }
+
+  /** A comparator for sorting event results by their ranking in descending order. */
+  public static final Comparator<EventResult> ORDER_BY_RANKING_DESC =
+      new Comparator<EventResult>() {
+        @Override
+        public int compare(EventResult a, EventResult b) {
+          return Float.compare(b.ranking, a.ranking);
+        }
+      };
+}
