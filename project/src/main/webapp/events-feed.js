5365dd8f732135b4dd7d1e60dfd8c40214696996
Stephanie Fu <fus@mit.edu>
2020-08-21 04:12:16 +0000

    Take out second document.ready function

--- project/src/main/webapp/events-feed.js
+++ project/src/main/webapp/events-feed.js
@@ -1,7 +1,3 @@
-$(function() {
-  populateEvents();
-});
-
 async function populateEvents() {
   const rankedEvents = await getRankedEvents();
   const eventLevels = getLodsFromEvents(rankedEvents);
@@ -11,6 +7,7 @@ async function populateEvents() {
 const filters = {}
 
 $(async function() {
+  populateEvents();
   toggleDropdown();
   // If want all events on discovery page 
   if (!(window.location.href).includes('filtered=true')) {
