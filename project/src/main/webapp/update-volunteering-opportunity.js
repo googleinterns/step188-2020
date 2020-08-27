9096a063bf9646d440bbc8021935f8df92bccd65
nikhitashanker <rocketns@gmail.com>
2020-08-27 23:59:40 +0000

    Style fixes.

--- project/src/main/webapp/update-volunteering-opportunity.js
+++ project/src/main/webapp/update-volunteering-opportunity.js
@@ -29,8 +29,8 @@ async function getVolunteeringOpportunityFormData() {
 
   const opportunityForm = document.getElementById('opportunity-form');
   opportunityForm.action =
-      `/volunteering-form-handler?opportunity-id=${opportunityId}`
-          + `&event-id=${eventId}`;
+      `/volunteering-form-handler?opportunity-id=${opportunityId}` +
+      `&event-id=${eventId}`;
 }
 
 /**
