cf1c0417edd69346e73e9513368bd3e01e8bd473
nikhitashanker <rocketns@gmail.com>
2020-08-31 23:38:43 +0000

    Fix database constant after database restore.

--- project/src/main/java/com/google/sps/utilities/DatabaseConstants.java
+++ project/src/main/java/com/google/sps/utilities/DatabaseConstants.java
@@ -2,6 +2,6 @@ package com.google.sps.utilities;
 
 public final class DatabaseConstants {
   public static final String INSTANCE_ID = "step-188-instance";
-  public static final String DATABASE_ID = "event-organizer-db";
+  public static final String DATABASE_ID = "event-organizer-db-1";
   public static final String PROJECT_ID = "event-organizer-step-2020";
 }
